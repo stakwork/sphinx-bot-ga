22816f21f6edb2e4aae44c55b4f6f44cb94d0033
Gonzalo Aune <gonzaloaune@gmail.com>
2024-01-27 10:57:55 +0000

    initial commit of github action

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const { http_client } = require('@actions/http-client')
 
 /**
  * The main function for the action.
@@ -7,18 +7,27 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const tribe_url = core.getInput('tribe_url', { required: true })
+    const bot_id = core.getInput('bot_id', { required: true })
+    const bot_secret = core.getInput('bot_secret', { required: true })
+    const chat_uuid = core.getInput('chat_uuid', { required: true })
+    const bot_message = core.getInput('bot_message', { required: true })
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Posting to ${tribe_url} ...`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const req_params = {
+      action: 'broadcast',
+      bot_id: bot_id,
+      bot_secret: bot_secret,
+      chat_uuid: chat_uuid,
+      content: bot_message
+    }
+
+    const restRes = await http_client.postJson(tribe_url, req_params)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('response', restRes.result.json)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
